77b021916b8857a1cefe334639df5d3c75e0ca9b
Shaowei <zsw22@vip.qq.com>
2015-10-29 16:47:08 +1100

    v0.5 数据的处理完成

---
+++ Navigation Course Scheduling/ScheduleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ScheduleViewController.swift
+//  Navigation Course Scheduling
+//
+//  Created by ShaoweiZhang on 15/10/29.
+//  Copyright © 2015年 ShaoweiZhang. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ScheduleViewController: ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
