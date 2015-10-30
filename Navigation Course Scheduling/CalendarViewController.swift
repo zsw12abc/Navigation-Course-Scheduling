a475f7f474005a7841815ba01b85e53d363e0b7a
Shaowei <zsw22@vip.qq.com>
2015-10-30 14:11:02 +1100

    排课输入完成

---
+++ Navigation Course Scheduling/CalendarViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CalendarViewController.swift
+//  Navigation Course Scheduling
+//
+//  Created by ShaoweiZhang on 15/10/30.
+//  Copyright © 2015年 ShaoweiZhang. All rights reserved.
+//
+
+import UIKit
+import EventKit
+import Parse
+
+class CalendarViewController: UIViewController {
+    var courseHours : Int?;
+    var roomNum : Int?;
+    var startDate : NSDate?
+    var endDate : NSDate?;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Navigation Course Scheduling/ScheduleViewController.swift
+++ Navigation Course Scheduling/ScheduleViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 import Parse
-import EventKit
 
 class ScheduleViewController: UIViewController {
 
+
+
+    @IBOutlet weak var courseHoursLabel: UILabel!
+    @IBOutlet weak var roomNumLabel: UILabel!
+    @IBOutlet weak var courseHoursTextField: UITextField!
+    @IBOutlet weak var roomNumTextField: UITextField!
+    @IBOutlet weak var startDatePicker: UIDatePicker!
+    @IBOutlet weak var endDatePicker: UIDatePicker!
+    
+    var courseHours : Int?;
+    var roomNum : Int?;
+    var startDate : NSDate?
+    var endDate : NSDate?;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        courseHoursLabel.text = "*课时:"
+        roomNumLabel.text = "*教室数量:"
+        courseHoursLabel.textColor = UIColor.redColor();
+        roomNumLabel.textColor = UIColor.redColor();
         // Do any additional setup after loading the view.
     }
 
@@ -33,5 +49,26 @@ class ScheduleViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
 
+
+    @IBAction func scheduleButtonPressed(sender: UIButton) {
+        if courseHoursTextField.text != "" && roomNumTextField.text != ""{
+            courseHours = Int(courseHoursTextField.text!);
+            roomNum = Int(roomNumTextField.text!);
+            startDate = startDatePicker.date;
+            endDate = endDatePicker.date;
+            performSegueWithIdentifier("calendarSegue", sender: sender);
+        }
+        
+    }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "calendarSegue" {
+            let vc = segue.destinationViewController as! CalendarViewController;
+            vc.courseHours = courseHours;
+            vc.roomNum = roomNum;
+            vc.startDate = startDate;
+            vc.endDate = endDate;
+        }
+    }
 }
