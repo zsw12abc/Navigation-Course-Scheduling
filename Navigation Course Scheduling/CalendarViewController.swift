950baca0fe1f5985b23194b5c336d4cc31239444
Shaowei <zsw22@vip.qq.com>
2015-10-30 15:17:28 +1100

    segue way to calendar VC

--- Navigation Course Scheduling/CalendarViewController.swift
+++ Navigation Course Scheduling/CalendarViewController.swift
@@ -15,9 +15,51 @@ class CalendarViewController: UIViewController {
     var roomNum : Int?;
     var startDate : NSDate?
     var endDate : NSDate?;
+    var courseList = Array<PFObject>();
+    var lecturerList = Array<PFObject>();
+    var studentList = Array<PFObject>();
+    var totalHours: Int = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let courseQuery = PFQuery(className: "Course");
+        let lecturerQuery = PFQuery(className: "Lecturer");
+        let studentQuery = PFQuery(className: "Student");
+        courseQuery.findObjectsInBackgroundWithBlock { (courses, error) -> Void in
+            if error == nil {
+               if let courses = courses{
+                    for course in courses {
+                        self.courseList.append(course);
+                        self.totalHours = self.totalHours + (course["hours"] as! Int);
+                    }
+                }
+                
+            }else{
+                print("courseQuery: \(error)");
+            }
+        }
+        lecturerQuery.findObjectsInBackgroundWithBlock { (lecturers, error) -> Void in
+            if error == nil {
+                if let lecturers = lecturers {
+                    for lecturer in lecturers{
+                        self.lecturerList.append(lecturer);
+                    }
+                }
+            } else {
+                print("lecturerQuery: \(error)");
+            }
+        }
+        studentQuery.findObjectsInBackgroundWithBlock { (students, error) -> Void in
+            if error == nil {
+                if let students = students {
+                    for student in  students {
+                        self.studentList.append(student);
+                    }
+                }
+            }else {
+                print("studentQuery: \(error)");
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
